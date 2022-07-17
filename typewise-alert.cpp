8156292da0df779581b18ab37fa6e5afe3e41cce
jayminchhabhaya <jayamin.chhabhaya11@gmail.com>
2022-07-17 11:29:57 +0530

    add changes

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,6 +1,5 @@
 #include "typewise-alert.h"
 #include <stdio.h>
-#include<string>
 
 void printMessage(std::string printstr)
 {
--- typewise-alert.h
+++ typewise-alert.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<string>
 const std::string EMAILID = "a.b@c.com";
 
 typedef enum {

2a951c9a37b97521c909c04bb76d80f83b677284
jayminchhabhaya <33538525+jayminchhabhaya@users.noreply.github.com>
2022-07-17 10:01:24 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,9 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
+BatteryCharacter batteryChar;
+batteryChar.coolingType = PASSIVE_COOLING;
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+   REQUIRE(checkAndAlert(TO_CONTROLLER,batteryChar,30));
 }
